4eb84fef31ad302c8c5b0e77d31411fe79663a01
jatur <turayjj@mail.uc.edu>
2024-10-07 12:11:27 -0400

    Init Commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,32 @@
+import java.util.Scanner;
+
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int age = 0;
+        Scanner in = new Scanner(System.in);
+
+
+        System.out.print("Enter your age: ");
+        if (in.hasNextInt()) {
+            age = in.nextInt();
+            in.nextLine();
+
+            if (age >= 21) {
+                System.out.println("you get a wristband");
+            } else if (age < 21) {
+                System.out.println("you don't get a  wristband");
+            }
+
+        }
     }
 }
+
+
+
+
+
+
+
+
+
